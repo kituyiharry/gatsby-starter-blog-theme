39aa8ff7d2f28c8ee85bcf1c208b43bce0c5dae8
Harry Kituyi <kituyiharry@gmail.com>
2020-03-07 16:37:41 +0800

    Bio-setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Dev-olutionary`,
+    author: `Harry K`,
+    description: `Just some stuff i'm upto...more posts on the way`,
     social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
-      },
+      //{
+        //name: `twitter`,
+        //url: `https://twitter.com/gatsbyjs`,
+      //},
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/kituyiharry`,
       },
     ],
   },
